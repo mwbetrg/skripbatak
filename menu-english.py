c6a04ff51ae57ab6fad8e643dc870770c12ab2e4
Joe Labi <mwbetrg@gmail.com>
2017-01-16 14:24:00 +0000

    Updated menu-english.py 20170116-1423

--- menu-english.py
+++ menu-english.py
@@ -45,7 +45,7 @@ if os.path.exists('/storage/extSdCard'):
     backupdir = '/storage/extSdCard/dbbackup/'
     db = '/storage/extSdCard/mydb/english-notes-exercises.sqlite'
 else:
-    database = SqliteDatabase('english-notes-exercises.sqlite', **{})
+    database = SqliteDatabase('/usb/termux/termux-git/db/english-notes-exercises.sqlite', **{})
 
 class BaseModel(Model):
     class Meta:
